1c2c253447ce44939d13b27bef4a7b4d4d1879d2
Andrii Ilin <andrey.ilyin83@gmail.com>
2024-08-26 22:39:09 -0600

    update main tf

--- main.tf
+++ main.tf
@@ -2,15 +2,16 @@ provider "flux" {
   kubernetes = {
     config_path = var.config_path
   }
-  git = {
-    url = "https://github.com/${var.github_repository}.git"
-    http = {
-      username = "git"
-      password = var.github_token
-    }
-  }
 }
 
 resource "flux_bootstrap_git" "this" {
+  url = "https://github.com/${var.github_repository}.git"
+  branch = "main"
   path = var.target_path
+  
+  auth = {
+    type = "http"
+    username = "git"
+    password = var.github_token
+  }
 }
